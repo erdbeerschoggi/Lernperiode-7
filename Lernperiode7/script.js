919b714ffebe47d5202614422fa728ebded8d53a
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2025-10-31 12:02:45 +0100

    Add files via upload

--- Lernperiode7/script.js
+++
@@ -1,8 +0,0 @@
-
-const button = document.getElementById("clickButton");
-const message = document.getElementById("message");
-
-
-button.addEventListener("click", () => {
-  message.textContent = "Proof of Concept";
-});
